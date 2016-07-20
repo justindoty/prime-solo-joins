1610b6bcddae2bba466138e26fd6aab02309204c
Justin Doty <justindoty12@gmail.com>
2016-07-20 10:40:13 -0500

    updated2 git commit -m updated2

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,10 +1,10 @@
 -- Get all customers and their addresses.
 
-SELECT * FROM customers CROSS JOIN addresses;
+SELECT * FROM customers JOIN addresses ON customers.id = addresses.customer_id;
 
 -- Get all orders and their line items.
 
-SELECT * FROM orders JOIN line_items ON oreders.id = line_items.order_id;
+SELECT * FROM orders JOIN line_items ON orders.id = line_items.order_id;
 
 -- Which warehouses have cheetos?
 SELECT warehouse FROM warehouse JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
@@ -21,6 +21,7 @@ WHERE products.description = 'diet pepsi';
 
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
+SELECT c.first_name, c.last_name AS name, count(o.id) as total_orders FROM customers AS c JOIN addresses AS a ON c.id = a.customer_id JOIN orders AS o ON a.id = o.address_id GROUP BY c.first_name, c.last_name;
 
 
 -- How many customers do we have?
